ba670dfea8a61753177856a138dee29775ebbe79
napmkmk <napmkmk@192.168.200.184>
2022-12-09 00:56:11 +0900

    글테스트수정

--- src/main/java/com/napmkmk/mkboard/entity/Question.java
+++ src/main/java/com/napmkmk/mkboard/entity/Question.java
@@ -3,7 +3,13 @@ package com.napmkmk.mkboard.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 
